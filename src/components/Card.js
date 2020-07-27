7fde285ed6c5f4301ae005c5f72eafd3471adf45
rokaizen <robertojavier.araneda@gmail.com>
2020-07-27 00:59:34 -0400

    props css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,30 @@
 import React from 'react'
-import logo from "../images/exercise.png"
+import excerciseImg from "../images/exercise.png"
+import circlesImg from "../images/circles.png"
+import "../styles/Card.css"
 
 class Card extends React.Component {
+
     render(){
+        const {title, description, img,leftColor,rightColor} = this.props
         return (
-            <div>
-                <div>
-                    <img src={logo}/>
+            <div className="card mx-auto Fitness-Card" 
+            style={{
+                backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor}, ${rightColor})`
+            }}
+            >
+                <div className="card-body">
+                    <div className="row center">
+                        <div className="col-6">
+                            <img src={img} className="float-right"/>
+                        </div>
+                        <div className="col-6 Fitness-Card-Info">
+                            <h1>{title}</h1>
+                            <p>{description}</p>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <h1>Technique Guides</h1>
-                    <p>Lorem</p>
-                </div>
-                
             </div>
-            
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Card from './components/Card'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const container = document.getElementById('root')
 
-ReactDOM.render(<Card></Card>,container)
+ReactDOM.render(<Card 
+                    title="Technique Guides"
+                    description="Lorem"
+                    img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+                    leftColor="#A74CF2"
+                    rightColor="#617BFB"
+/>,container)
