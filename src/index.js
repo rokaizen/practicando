c1cf66856b8a0815032c1463304736a71ce87eed
rokaizen <robertojavier.araneda@gmail.com>
2020-07-28 01:24:05 -0400

    props and states

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Card from './components/Card'
+import Welcome from './components/Welcome'
 import 'bootstrap/dist/css/bootstrap.css'
 
 const container = document.getElementById('root')
 
-ReactDOM.render(<Card 
+ReactDOM.render(
+            <div>
+                 <Welcome 
+                 username="Rocko"
+                 />
+                 <Card 
                     title="Technique Guides"
                     description="Lorem"
                     img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
                     leftColor="#A74CF2"
-                    rightColor="#617BFB"
-/>,container)
+                    rightColor="#617BFB"/>
+            </div>
+,container)
